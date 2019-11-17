f025301d466ac69aa40e160fa7579a75a50062dd
rakhmadi D <rakhmadiwalker@gmail.com>
2019-11-17 14:43:26 +0700

    page_dynamic

--- page.php
+++ page.php
@@ -12,20 +12,48 @@
     <nav class="nav white shadow relative">
     <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
     </nav>
-    <div class='jumbo'></div>
+    
     <div class="row">
-    <div class='col-3'><div class='card'></div></div>
-    <div class='col-3'><div class='mr-5'></div></div>
-    <div class='col-3'><div class='mr-5'></div></div>
-    <div class='col-2'><div class='mr-5'></div></div>
-    <div class='col-2'><div class='mr-5'></div></div>
-
-    <?php
-       for ($i=0; $i < 1000; $i++) { 
-           echo"<div class='col-4  '><div class='mr-5'>loop</div></div>";
-       }
-      ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="header">
+<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
+</header>
+<div class="entry-content">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
+</div>
+</article>
+<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
+<?php endwhile; endif; ?>
     </div>
+    <div class="row primary bawah">
+        <div class="footer bawah">
+            <div class="col-3">
+               <div class="c">
+               <ul>
+                    <li><a href="http://">chec</a></li>
+                </ul>
+               </div>
+            </div>
+            <div class="col-3">
+            <div class="c">
+               <ul>
+               <li><a href="http://">chec</a></li>
+                </ul>
+               </div>
+            </div>
+            <div class="col-3">
+            <div class="c">
+               <ul>
+               <li><a href="http://">chec</a></li>
+                </ul>
+               </div>
+            </div>
+        </div>
+    </div>
+   
     <div class="mid-bt" onclick="show()">
        <p>></p>
     </div>
--- single.php
+++ single.php
@@ -1,12 +1,64 @@
-<?php get_header(); ?>
-<main id="content">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+                         
+    <?php wp_head();?>
+</head>
+<body>
+    <nav class="nav white shadow relative">
+    <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+    </nav>
+    
+    <div class="row">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
-<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
+<?php if ( comments_open() && ! post_password_required() ) { comments_template( '', false ); } ?>
 <?php endwhile; endif; ?>
-<footer class="footer">
-<?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+    </div>
+    <div class="row primary">
+        <div class="footer">
+            <div class="col-3">
+               <div class="c">
+               <ul>
+                    <li><a href="http://">chec</a></li>
+                </ul>
+               </div>
+            </div>
+            <div class="col-3">
+            <div class="c">
+               <ul>
+               <li><a href="http://">chec</a></li>
+                </ul>
+               </div>
+            </div>
+            <div class="col-3">
+            <div class="c">
+               <ul>
+               <li><a href="http://">chec</a></li>
+                </ul>
+               </div>
+            </div>
+        </div>
+    </div>
+   
+    <div class="mid-bt" onclick="show()">
+       <p>></p>
+    </div>
+    <div id="t" onclick="hide()" class="slide-k">
+    <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+   </div>
+    <script>
+    function show(){
+        document.getElementById('t').style.width="50%";
+    }
+    function hide(){
+        document.getElementById('t').style.width="0";
+    }
+    </script>
+</body>
+</html>
